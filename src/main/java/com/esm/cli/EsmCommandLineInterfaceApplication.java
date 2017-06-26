2d09889ced5e20230234b58f16218ae69a6a3b8f
Durgesh RAI <drai548@gmail.com>
2017-06-26 17:02:01 +0530

    initial commit

---
+++ src/main/java/com/esm/cli/EsmCommandLineInterfaceApplication.java
@@ -0,0 +1,44 @@
+package com.esm.cli;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
+import org.springframework.security.oauth2.common.AuthenticationScheme;
+
+@SpringBootApplication
+public class EsmCommandLineInterfaceApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsmCommandLineInterfaceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("Starting cron job");
+
+		ResourceOwnerPasswordResourceDetails details = new ResourceOwnerPasswordResourceDetails();
+		details.setClientAuthenticationScheme(AuthenticationScheme.header);
+		details.setAccessTokenUri("http://localhost:8888/services/oauth/token");
+
+		details.setScope(Arrays.asList("employee_read", "employee_write", "employee_trust"));
+
+		details.setClientId("employee-service");
+		details.setClientSecret("employee-service-secret");
+
+		details.setUsername("durgesh");
+		details.setPassword("durgesh");
+
+		OAuth2RestTemplate template = new OAuth2RestTemplate(details);
+
+		String token = template.getAccessToken().getValue();
+		System.out.println("Token = " + token);
+
+		String data = template.getForObject("http://localhost:9999/services/message", String.class);
+		System.out.println("Data = " + data);
+	}
+}
---
+++ src/test/java/com/esm/cli/EsmCommandLineInterfaceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.esm.cli;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EsmCommandLineInterfaceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
