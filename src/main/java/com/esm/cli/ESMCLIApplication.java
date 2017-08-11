39cd24eb0715e1580c62325534e2b383c24a97e2
Durgesh RAI <drai548@gmail.com>
2017-08-12 00:01:32 +0530

    Updated the oauth configuration

--- src/main/java/com/esm/cli/ESMCLIApplication.java
+++ src/main/java/com/esm/cli/ESMCLIApplication.java
@@ -5,40 +5,54 @@ import java.util.Arrays;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
 import org.springframework.security.oauth2.common.AuthenticationScheme;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @SpringBootApplication
+@EnableOAuth2Client
 public class ESMCLIApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ESMCLIApplication.class, args);
 	}
+	
+	@Bean
+	public OAuth2RestTemplate restTemplate() {
+		return new OAuth2RestTemplate(resource(), new DefaultOAuth2ClientContext());
+	}
+
+	@Bean
+	protected OAuth2ProtectedResourceDetails resource() {
+		ResourceOwnerPasswordResourceDetails details = new ResourceOwnerPasswordResourceDetails();
+		details.setScope(Arrays.asList("read", "write"));
+		details.setClientAuthenticationScheme(AuthenticationScheme.header);
+		details.setAccessTokenUri("http://localhost:9999/oauth2/services/oauth/token");
+		details.setClientId("commandlineinterface");
+		details.setClientSecret("commandlineinterface");
+		details.setGrantType("password");
+		details.setUsername("user");
+		details.setPassword("password");
+		return details;
+	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		System.out.println("Starting cron job");
 
-		ResourceOwnerPasswordResourceDetails details = new ResourceOwnerPasswordResourceDetails();
-		details.setClientAuthenticationScheme(AuthenticationScheme.header);
-		details.setAccessTokenUri("http://localhost:8888/services/oauth/token");
+		System.out.println("Starting cron job ::==>");
 
-		details.setScope(Arrays.asList("employee_read", "employee_write", "employee_trust"));
-
-		details.setClientId("employee-service");
-		details.setClientSecret("employee-service-secret");
-
-		details.setUsername("durgesh");
-		details.setPassword("durgesh");
-
-		OAuth2RestTemplate template = new OAuth2RestTemplate(details);
+		OAuth2RestTemplate template = restTemplate();
 
 		String token = template.getAccessToken().getValue();
 		System.out.println("Token = " + token);
 
 		String data = template.getForObject("http://localhost:9090/services/message", String.class);
-		System.out.println("Data = " + data);
+		System.out.println("Account Data ==> " + data);
+
+		System.out.println("==============================================================================");
 	}
 }
